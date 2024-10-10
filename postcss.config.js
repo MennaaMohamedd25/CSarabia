99be83d927cc8c28a3ca718b37fc02f2c57006eb
MennaaMohamedd25 <mennaa.mohamedd24@gmail.com>
2024-10-11 02:43:00 +0300

    first commit

--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,8 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
+  rules: {
+    'no-unused-vars': process.env.CI ? ['warn'] : ['error'],
+    'jsx-a11y/iframe-has-title': process.env.CI ? ['warn'] : ['error'],
+  }
 }
